3f6782cd8599005fa44fd89dce19a5fdc30de0d1
SCOPE <olajuwonrichard22@gmail.com>
2024-03-21 20:21:44 +0100

    made name change in my ads component

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -2,7 +2,7 @@ export default function Ads()
 {
     return(
         <div style={{width: '100vw', textAlign: 'center', position:'fixed', bottom: '0', background: 'black'}}>
-            Developed by Scope
+            Developed by Richard
         </div>
     )
 }
